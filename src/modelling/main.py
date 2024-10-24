1fd41992fe0e2a803cf1623651f68dbc0dc8f9a3
antoventura <antonio.ventu@hotmail.it>
2024-10-24 21:17:29 +0200

    fix: update modelling module with right import

--- src/modelling/main.py
+++ src/modelling/main.py
@@ -1,48 +1,59 @@
+"""Main module to run whole model."""
+import argparse
+import os
 from pathlib import Path
+
 from load_data import download_data, load_data
-from predicting import predict, evaluate_rmse
-from preprocessing import get_preprocessing_pipeline, compute_target, split_data
+from predicting import evaluate_rmse, predict
+from preprocessing import compute_target, get_preprocessing_pipeline, split_data  # noqa
 from training import fit_random_forest
 from utils import pickle_obj
 
-import argparse
-
-path = download_data()
 
 def main(trainset_path: Path) -> None:
-    """Train a model using the data at the given path and save the model (pickle)."""
+    """Train a model using the data at the given path."""
     # Read data
     df = load_data(trainset_path)
 
     # Preprocess data
-    preprocessor = get_preprocessing_pipeline(df)
-    X, y = compute_target(df, target_column="Rings")
-    X_train, X_test, y_train, y_test = split_data(X, y)
+    x, y = compute_target(df, target_column="Rings")
+    preprocessor = get_preprocessing_pipeline(x)
 
-    X_train_preprocessed = preprocessor.fit_transform(X_train)
-    X_test_preprocessed = preprocessor.transform(X_test)
+    x_train, x_test, y_train, y_test = split_data(x, y)
+    x_train_preprocessed = preprocessor.fit_transform(x_train)
+    x_test_preprocessed = preprocessor.transform(x_test)
 
     # Pickle encoder
-    pickle_obj(obj=preprocessor, filepath="web_service/local_objects/preprocessor.pkl")
+    pickle_obj(obj=preprocessor, filepath="../web_service/local_objects/preprocessor.pkl")  # noqa
 
     # Train model
-    model = fit_random_forest(X_train_preprocessed, y_train)
+    model = fit_random_forest(x_train_preprocessed, y_train)
 
-    # Pickle model --> The model should be saved in pkl format the `src/web_service/local_objects` folder
-    pickle_obj(obj=model, filepath="web_service/local_objects/random_forest.pkl")
+    pickle_obj(obj=model, filepath="../web_service/local_objects/random_forest.pkl")  # noqa
 
     # Predict
-    y_pred = predict(model, X_test_preprocessed)
+    y_pred = predict(model, x_test_preprocessed)
 
     # Compute RMSE and display it
     rmse = evaluate_rmse(y_test, y_pred)
 
-    print(f'The RandomForest predicted age with an RMSE of {rmse}.')
+    print(f"The RandomForest predicted age with an RMSE of {rmse}.")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train a model using the data at the given path.")
-    parser.add_argument("trainset_path", type=str, default="abalone-dataset/abalone.csv", help="Path to the training set")
-    args = parser.parse_args()
-    main(args.trainset_path)
+    data_folder = "abalone-dataset"
+    csv_file = os.path.join(data_folder, "abalone.csv")
 
+    # Check if the data file exists, download if it doesn't
+    if not os.path.exists(csv_file):
+        download_data(force=False)
+    parser = argparse.ArgumentParser(description="Train a model using the data at the given path.")  # noqa
+    parser.add_argument(
+        "trainset_path",
+        type=str,
+        nargs="?",
+        default="abalone-dataset/abalone.csv",
+        help="Path to the training set",  # noqa
+    )
+    args = parser.parse_args()
+    main(trainset_path=args.trainset_path)
