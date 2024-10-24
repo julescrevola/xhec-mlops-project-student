c5b400301cea414f82e62eebb4cd9b10e313bfc8
Jules Crevola <julescrevola@gmail.com>
2024-10-24 19:00:05 +0200

    fix: changed pickle functions

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -2,12 +2,12 @@ from typing import Any
 
 import pickle
 
-def load_pickle(path: str):
-    with open(path, "rb") as f:
-        loaded_obj = pickle.load(f)
-    return loaded_obj
+def pickle_obj(obj, filepath: str) -> None:
+    with open(filepath, "wb") as f:
+        pickle.dump(obj, f)
 
 
-def save_pickle(path: str, obj: Any):
-    with open(path, "wb") as f:
-        return pickle.dump(obj, f)
+def load_pickle(filepath: str):
+    with open(filepath, "rb") as f:
+        model = pickle.load(f)
+    return model
