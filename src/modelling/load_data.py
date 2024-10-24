e29988d5c780cd463ba099f6c4799f57c176ad04
TheCodingCvrlo <carlo-antonio.patti@hec.edu>
2024-10-24 21:47:01 +0200

    format: added docstrings

--- src/modelling/load_data.py
+++ src/modelling/load_data.py
@@ -1,12 +1,18 @@
+"""Load the data from Kaggle."""
+
 import os
+
 import opendatasets as od
 import pandas as pd
 
+
 def download_data(force: bool = False) -> str:
-    od.download("https://www.kaggle.com/datasets/rodolfomendes/abalone-dataset", force=force)
+    """Download the data from Kaggle."""
+    od.download("https://www.kaggle.com/datasets/rodolfomendes/abalone-dataset", force=force)  # noqa
     data_folder = "abalone-dataset"
     return os.path.join(data_folder, "abalone.csv")
 
 
-def load_data(PATH: str) -> pd.DataFrame:
-    return pd.read_csv(PATH)
+def load_data(path: str) -> pd.DataFrame:
+    """Load the data from the given path."""
+    return pd.read_csv(path)
--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,13 +1,17 @@
-from typing import Any 
+"""Utility functions for the modelling module."""
+
 
 import pickle
 
+
 def pickle_obj(obj, filepath: str) -> None:
+    """Pickle an object to a file."""
     with open(filepath, "wb") as f:
         pickle.dump(obj, f)
 
 
 def load_pickle(filepath: str):
+    """Load a pickled object from a file."""
     with open(filepath, "rb") as f:
         model = pickle.load(f)
     return model
