20d804010b52c00b00d4084f3fa15eea53e1a011
TheCodingCvrlo <carlo-antonio.patti@hec.edu>
2024-10-24 21:58:44 +0200

    fix: flake8 formatting

--- src/modelling/predicting.py
+++ src/modelling/predicting.py
@@ -1,18 +1,19 @@
+"""Run predictions using the fitted Random Forest regression model."""
+
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
 
-def predict(model: RandomForestRegressor, X_new: pd.DataFrame) -> pd.Series:
-    """
-    Make predictions using the fitted Random Forest regression model.
+def predict(model: RandomForestRegressor, x_new: pd.DataFrame) -> pd.Series:
+    """Make predictions using the fitted Random Forest regression model.
 
     Parameters:
     ----------
     model : RandomForestRegressor
         The fitted Random Forest regression model.
-    X_new : pd.DataFrame
+    x_new : pd.DataFrame
         The new feature data for which predictions are to be made.
 
     Returns:
@@ -20,8 +21,7 @@ def predict(model: RandomForestRegressor, X_new: pd.DataFrame) -> pd.Series:
     pd.Series
         The predicted values for the new data.
     """
-
-    return model.predict(X_new)
+    return model.predict(x_new)
 
 
 def evaluate_rmse(y_true: pd.Series, y_pred: pd.Series) -> float:
@@ -40,6 +40,5 @@ def evaluate_rmse(y_true: pd.Series, y_pred: pd.Series) -> float:
     float
         The RMSE value.
     """
-
     rmse = np.sqrt(mean_squared_error(y_true, y_pred))
     return rmse
--- src/modelling/preprocessing.py
+++ src/modelling/preprocessing.py
@@ -1,20 +1,28 @@
+"""Preprocessing functions for the data."""
+
+from typing import Tuple
+
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from typing import Tuple
 
 
-def get_preprocessing_pipeline(X: pd.DataFrame) -> ColumnTransformer:
+def get_preprocessing_pipeline(x: pd.DataFrame) -> ColumnTransformer:
     """Create a preprocessing pipeline for the data."""
     # identify categorical and numerical columns
-    categorical_cols = X.select_dtypes(include=["object", "category"]).columns.tolist()
-    numerical_cols = X.select_dtypes(include=["number"]).columns.tolist()
+    categorical_cols = x.select_dtypes(include=["object", "category"]).columns.tolist()  # noqa
+    numerical_cols = x.select_dtypes(include=["number"]).columns.tolist()
 
     # preprocessing pipeline for numerical features
-    numerical_pipeline = Pipeline(steps=[("imputer", SimpleImputer(strategy="mean")), ("scaler", StandardScaler())])
+    numerical_pipeline = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="mean")),
+            ("scaler", StandardScaler()),
+        ]
+    )
 
     # preprocessing pipeline for categorical features
     categorical_pipeline = Pipeline(
@@ -33,18 +41,23 @@ def get_preprocessing_pipeline(X: pd.DataFrame) -> ColumnTransformer:
 
     return preprocessor
 
-def compute_target(df: pd.DataFrame, target_column: str) -> Tuple[pd.DataFrame, pd.Series]:
 
+def compute_target(df: pd.DataFrame, target_column: str) -> Tuple[pd.DataFrame, pd.Series]:  # noqa
+    """Separate features and target variable."""
     # separate features and target variable
-    X = df.drop(columns=[target_column])
+    x = df.drop(columns=[target_column])
     y = df[target_column] + 1.5
 
-    return X, y
+    return x, y
+
 
 def split_data(
-    X: pd.DataFrame, y: pd.Series, test_size: float = 0.2, random_state: int = 42
+    x: pd.DataFrame,
+    y: pd.Series,
+    test_size: float = 0.2,
+    random_state: int = 42,
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """Split the data into training and test sets."""
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=random_state)  # noqa
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-
-    return X_train, X_test, y_train, y_test
+    return x_train, x_test, y_train, y_test
