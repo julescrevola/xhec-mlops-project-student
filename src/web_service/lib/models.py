8252b0849042e65cb6d513c3470d0036c4ff9d49
TheCodingCvrlo <carlo-antonio.patti@hec.edu>
2024-10-24 23:21:59 +0200

    moved modules to fastapi framework

--- src/web_service/lib/models.py
+++ src/web_service/lib/models.py
@@ -1,1 +1,16 @@
-# Pydantic models for the web service
+"""Pydantic models for the web service."""
+
+from pydantic import BaseModel
+
+
+class AbaloneData(BaseModel):
+    """Pydantic model for the input data to the web service."""
+
+    Length: float
+    Diameter: float
+    Height: float
+    Whole_weight: float
+    Shucked_weight: float
+    Viscera_weight: float
+    Shell_weight: float
+    Sex: str
--- src/web_service/utils.py
+++ src/web_service/utils.py
@@ -1,1 +1,17 @@
-# Utils file. TODO: add a `load_object` function to load pickle objects
+"""Utility functions for the modelling module."""
+
+
+import pickle
+
+
+def pickle_obj(obj, filepath: str) -> None:
+    """Pickle an object to a file."""
+    with open(filepath, "wb") as f:
+        pickle.dump(obj, f)
+
+
+def load_object(filepath: str):
+    """Load a pickled object from a file."""
+    with open(filepath, "rb") as f:
+        model = pickle.load(f)
+    return model
